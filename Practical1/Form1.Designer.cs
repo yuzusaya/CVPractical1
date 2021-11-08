7fe2c64f0528a8f99a2724dad56cdf0c348468d6
沙耶 <yhsia@codersai.com>
2021-11-08 11:45:28 +0800

    Add project files.

---
+++ Practical1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Practical1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.CoordinateLabel = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblThreshold = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(27, 358);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(103, 48);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Search";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(499, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(499, 358);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(153, 48);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Grayscaling";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // CoordinateLabel
+            // 
+            this.CoordinateLabel.AutoSize = true;
+            this.CoordinateLabel.Location = new System.Drawing.Point(496, 432);
+            this.CoordinateLabel.Name = "CoordinateLabel";
+            this.CoordinateLabel.Size = new System.Drawing.Size(0, 17);
+            this.CoordinateLabel.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(979, 34);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(1132, 358);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(147, 48);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "Binarize";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(1406, 34);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1406, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Contrast Enhance";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblThreshold
+            // 
+            this.lblThreshold.Location = new System.Drawing.Point(979, 368);
+            this.lblThreshold.Name = "lblThreshold";
+            this.lblThreshold.Size = new System.Drawing.Size(113, 22);
+            this.lblThreshold.TabIndex = 10;
+            this.lblThreshold.Text = "128";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1789, 652);
+            this.Controls.Add(this.lblThreshold);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.CoordinateLabel);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label CoordinateLabel;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox lblThreshold;
+    }
+}
+
---
+++ Practical1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using Image = System.Drawing.Image;
+
+namespace Practical1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _image;
+        private Bitmap _grayImage;
+        private int threshold = 128;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                _image = (Bitmap)Image.FromFile(Openfile.FileName);
+                pictureBox1.Image = _image;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Grayscale grayFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            _grayImage = grayFilter.Apply(_image);
+            pictureBox2.Image = _grayImage;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                try
+                {
+                    Color color = _grayImage.GetPixel(e.X, e.Y);
+                    CoordinateLabel.Text = $"({e.X},{e.Y}) : {color.ToString()}";
+                }
+                catch (ArgumentOutOfRangeException arg)
+                {
+                    //ignore the error
+                }
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (_grayImage == null)
+                return;
+
+            if (int.TryParse(lblThreshold.Text, out threshold))
+            {
+                if(threshold < 0 || threshold > 255)
+                {
+                    return;
+                }
+                Threshold thresholdValue = new Threshold(threshold);
+                Bitmap binaryImage = thresholdValue.Apply(_grayImage);
+                pictureBox3.Image = binaryImage;
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_grayImage == null)
+                return;
+            HistogramEqualization histEq = new HistogramEqualization();
+            Bitmap histEqImage = histEq.Apply(_grayImage);
+            pictureBox4.Image = histEqImage;
+        }
+    }
+}
